ba6b5629c1918d83dba73e4c0cd2b338274cfd56
Theodore Stites <argionad@gmail.com>
2022-09-05 12:35:12 -0400

    Add EmployeeDetails.kt to NavController

--- app/src/main/java/com/tstites/myhr/Navigation.kt
+++ app/src/main/java/com/tstites/myhr/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.tstites.myhr.ui.screens.EmployeeDetails
 import com.tstites.myhr.ui.screens.EmployeeList
 import com.tstites.myhr.ui.screens.HomeScreen
 import com.tstites.myhr.ui.screens.Screens
@@ -19,6 +20,10 @@ fun Navigation() {
         composable(Screens.EmployeeList.route) {
             val eList = EmployeeList()
             eList.EmployeeListLayout(navController = navController)
+        }
+        composable(Screens.EmployeeDetails.route) {
+            val eDetails = EmployeeDetails()
+            eDetails.EmployeeDetailsLayout(navController = navController)
         }
     }
 }
