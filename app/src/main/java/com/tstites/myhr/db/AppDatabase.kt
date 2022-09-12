01179f383c65d47dfa0234730f57c3f33bf0773c
Theodore Stites <argionad@gmail.com>
2022-09-12 16:45:29 -0400

    Add Room Entity and Dao to AppDatabase.kt

--- app/src/main/java/com/tstites/myhr/db/AppDatabase.kt
+++ app/src/main/java/com/tstites/myhr/db/AppDatabase.kt
@@ -6,10 +6,12 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.tstites.myhr.obj.Employee
 import com.tstites.myhr.obj.EmployeeDao
+import com.tstites.myhr.obj.ProjectDao
 
 @Database(entities = [Employee::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun employeeDao(): EmployeeDao
+    abstract fun projectDao(): ProjectDao
 }
 class DBConnection {
     fun buildDB(context: Context): AppDatabase {
