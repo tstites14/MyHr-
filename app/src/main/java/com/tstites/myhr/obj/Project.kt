539d3608443caa856f96979a6a71c3364687eba5
Theodore Stites <argionad@gmail.com>
2022-09-12 16:14:46 -0400

    Add Project and ProjectEmployee data classes and their respective DAO interfaces

---
+++ app/src/main/java/com/tstites/myhr/obj/Project.kt
@@ -0,0 +1,61 @@
+package com.tstites.myhr.obj
+
+import androidx.room.*
+
+@Entity
+data class Project (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "progress") val progress: Float = 0f,
+)
+
+@Entity(primaryKeys = ["employeeID", "projectID"] )
+data class ProjectEmployee (
+    val employeeID: Int,
+    val projectID: Int
+)
+
+@Dao
+interface ProjectDao {
+    @Query("SELECT * FROM Project")
+    fun getAllProjects(): List<Project>
+
+    @Query("SELECT * FROM Project WHERE id = :id")
+    fun getProjectById(id: Int): Project
+
+    @Query("SELECT * FROM Project WHERE title = :title")
+    fun getProjectByTitle(title: String?): List<Project>
+
+    @Query("SELECT * FROM Project WHERE progress >= :minCompletion")
+    fun getProjectByMinCompletion(minCompletion: Float): List<Project>
+
+    @Query("SELECT COUNT(*) FROM Project")
+    fun getTableEntries(): Int
+
+    @Insert
+    fun insertNewProject(vararg project: Project)
+
+    @Update
+    fun updateExistingProject(project: Project)
+
+    @Delete
+    fun deleteProject(projectID: Int): Int
+}
+
+@Dao
+interface ProjectEmployeeDao {
+    @Query("SELECT * FROM ProjectEmployee")
+    fun getAllProjectEmployees(): ProjectEmployee
+
+    @Query("SELECT * FROM ProjectEmployee WHERE projectID = :projectID")
+    fun getEmployeeIDsByProject(projectID: Int): List<ProjectEmployee>
+
+    @Query("SELECT * FROM ProjectEmployee WHERE employeeID = :employeeID")
+    fun getProjectIDsByEmployee(employeeID: Int): List<ProjectEmployee>
+
+    @Insert
+    fun insertNewProjectEmployee(vararg projectEmployee: ProjectEmployee)
+
+    @Delete
+    fun deleteProjectEmployee(projectEmployee: ProjectEmployee): Int
+}
