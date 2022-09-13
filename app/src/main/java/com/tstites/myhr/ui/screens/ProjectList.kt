db694c339d420acc5032f37607495d1997c485d9
Theodore Stites <argionad@gmail.com>
2022-09-13 14:56:40 -0400

    Add content to Cards in ProjectList.kt

--- app/src/main/java/com/tstites/myhr/ui/screens/ProjectList.kt
+++ app/src/main/java/com/tstites/myhr/ui/screens/ProjectList.kt
@@ -1,15 +1,20 @@
 package com.tstites.myhr.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.tstites.myhr.db.DBConnection
@@ -45,21 +50,38 @@ class ProjectList {
                 items(data, itemContent = { project ->
                     Row(modifier = Modifier.fillMaxWidth()) {
                         //A Card is essentially a fancy list item that contains its own UI
-                        Card {
-                            //TODO
+                        Card(
+                            shape = RoundedCornerShape(4.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .clickable {
+                                    //TODO: INSERT NAV HERE
+                                }
+                        ) {
+                            Column {
+                                Text(project.title ?: "Missing Data", style = TextStyle(fontSize = 20.sp),
+                                    modifier = Modifier.padding(16.dp))
+                                LinearProgressIndicator(progress = project.progress,
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .height(16.dp)
+                                        .fillMaxWidth())
+                            }
                         }
                     }
                 })
             }
-        }
+
             //Underneath the list is the bar that contains the searching, filtering, and adding buttons
             BottomAppBar(backgroundColor = MaterialTheme.colors.primarySurface,
                 contentColor = contentColorFor(SnackbarDefaults.backgroundColor),
                 elevation = AppBarDefaults.BottomAppBarElevation,
                 content = {
-                    //TODO
-            })
+                    //TODO: ADD SEARCH, FILTER, AND ADD BUTTONS HERE
+                })
         }
+    }
 
     @Preview
     @Composable
@@ -80,8 +102,9 @@ class ProjectList {
     private fun insertData(projectDao: ProjectDao) {
         if (projectDao.getTableEntries() == 0) {
             val p1 = Project(0, "MyHr+", 0.33f)
+            val p2 = Project(0, "Docx to Pdf Converter", 0f)
 
-            projectDao.insertNewProject(p1)
+            projectDao.insertNewProject(p1, p2)
         }
     }
 }
