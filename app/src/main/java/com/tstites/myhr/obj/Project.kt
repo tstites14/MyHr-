69ed2979bdd362223493d258212a284e408243fc
Theodore Stites <argionad@gmail.com>
2022-09-13 12:43:23 -0400

    Fix compile-time error with deleteProject parameter being the wrong type

--- app/src/main/java/com/tstites/myhr/obj/Project.kt
+++ app/src/main/java/com/tstites/myhr/obj/Project.kt
@@ -39,7 +39,7 @@ interface ProjectDao {
     fun updateExistingProject(project: Project)
 
     @Delete
-    fun deleteProject(projectID: Int): Int
+    fun deleteProject(project: Project): Int
 }
 
 @Dao
