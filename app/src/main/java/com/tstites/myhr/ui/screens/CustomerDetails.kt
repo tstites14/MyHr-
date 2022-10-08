f21da20a030688c42311e152f78f9714702d3b94
Theodore Stites <argionad@gmail.com>
2022-10-08 16:54:30 -0400

    Add UI to CustomerDetails.kt

--- app/src/main/java/com/tstites/myhr/ui/screens/CustomerDetails.kt
+++ app/src/main/java/com/tstites/myhr/ui/screens/CustomerDetails.kt
@@ -6,18 +6,23 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.tstites.myhr.db.DBConnection
 import com.tstites.myhr.obj.Customer
+import com.tstites.myhr.obj.CustomerDao
 import com.tstites.myhr.obj.CustomerProjectDao
 import com.tstites.myhr.obj.Project
 import com.tstites.myhr.ui.components.CommonElements
@@ -60,9 +65,9 @@ class CustomerDetails {
             }
 
             when(currentTab.value) {
-                Rows.Details -> cDao.updateExisting(customerInfo(commonElements, data, context))
+                Rows.Details -> cDao.updateExisting(customerInfo(commonElements, data, context, cDao, navController))
                 Rows.Projects ->  {
-                    val projects = customerProjects(commonElements, data.id, cpDao).toTypedArray()
+                    val projects = customerProjects(data.id, cpDao, navController).toTypedArray()
                     pDao.updateExistingProject(*projects)
                 }
             }
@@ -70,16 +75,17 @@ class CustomerDetails {
     }
 
     @Composable
-    fun customerInfo(common: CommonElements, customer: Customer, context: Context): Customer {
+    fun customerInfo(common: CommonElements, customer: Customer, context: Context, cDao: CustomerDao,
+                     navController: NavController): Customer {
         val textBoxState = remember { mutableStateOf(false) }
         val alertActive = remember { mutableStateOf(false) }
 
         Column(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.93f)) {
+            .fillMaxSize()) {
             Card(modifier = Modifier
                 .padding(12.dp)
-                .fillMaxWidth()) {
+                .fillMaxWidth()
+                .fillMaxHeight(0.85f)) {
                 Column(modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(),
@@ -92,9 +98,60 @@ class CustomerDetails {
                             state = textBoxState.value) {
                         customer.name = it
                     }
+                    common.DefaultTextField(
+                            text = customer.address ?: "",
+                            placeholder = "Address",
+                            title = "Address",
+                            modifier = Modifier.padding(8.dp),
+                            state = textBoxState.value) {
+                        customer.address = it
+                    }
+                    common.DefaultTextField(
+                        text = customer.city ?: "",
+                        placeholder = "City",
+                        title = "City",
+                        modifier = Modifier.padding(8.dp),
+                        state = textBoxState.value) {
+                        customer.city = it
+                    }
+                    common.DefaultTextField(
+                        text = customer.state ?: "",
+                        placeholder = "State",
+                        title = "State",
+                        modifier = Modifier.padding(8.dp),
+                        state = textBoxState.value) {
+                        customer.state = it
+                    }
+
                     common.DefaultButton(text = "Contact Info", modifier = Modifier.padding(8.dp)) {
                         alertActive.value = true
                     }
+                }
+            }
+
+            Row(modifier = Modifier
+                .fillMaxWidth()) {
+                val editButtonText = remember{ mutableStateOf("Edit") }
+
+                if (textBoxState.value) {
+                    editButtonText.value = "Save"
+                }
+
+                common.DefaultButton(text = editButtonText.value, modifier = Modifier
+                                                               .padding(12.dp)
+                                                               .weight(0.5f)) {
+                    if (editButtonText.value.lowercase() == "save") {
+                        cDao.updateExisting(customer)
+
+                        navController.navigate(Screens.CustomerList.route)
+                    }
+
+                    textBoxState.value = true
+                }
+                common.DefaultButton(text = "Delete Entry", modifier = Modifier
+                                                                       .padding(12.dp)
+                                                                       .weight(0.5f)) {
+
                 }
             }
         }
@@ -105,8 +162,8 @@ class CustomerDetails {
                     Column(modifier = Modifier.padding(8.dp),
                            horizontalAlignment = Alignment.CenterHorizontally) {
                         common.DefaultButton(text = "Email", modifier = Modifier
-                                                                        .padding(8.dp)
-                                                                        .fillMaxWidth()) {
+                            .padding(8.dp)
+                            .fillMaxWidth()) {
                             //Close alert before the email app opens
                             alertActive.value = false
 
@@ -126,8 +183,8 @@ class CustomerDetails {
 
                         }
                         common.DefaultButton(text = "Phone", modifier = Modifier
-                                                                        .padding(8.dp)
-                                                                        .fillMaxWidth()) {
+                            .padding(8.dp)
+                            .fillMaxWidth()) {
                             val phone = customer.phone
 
                             val intent = Intent(Intent.ACTION_DIAL)
@@ -143,12 +200,31 @@ class CustomerDetails {
     }
 
     @Composable
-    fun customerProjects(common: CommonElements, customerID: Int,
-                         customerProjectDao: CustomerProjectDao): List<Project> {
+    fun customerProjects(customerID: Int,
+                         customerProjectDao: CustomerProjectDao, navController: NavController): List<Project> {
         val projectList = customerProjectDao.selectProjectsByCustomerID(customerID)
 
         Column(modifier = Modifier.padding(12.dp)) {
-            Text("Hello, ${projectList[0].title}")
+            projectList.forEach { project ->
+                Card(shape = RoundedCornerShape(4.dp), modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clickable {
+                        navController.navigate(Screens.ProjectDetails.route + "/${project.id}")
+                    }) {
+                    Column(modifier = Modifier.fillMaxWidth()) {
+                        Text(project.title ?: "",
+                            modifier = Modifier.padding(8.dp),
+                            style = TextStyle(fontSize = 24.sp))
+
+                        LinearProgressIndicator(progress = project.progress,
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .height(16.dp)
+                                .fillMaxWidth())
+                    }
+                }
+            }
         }
 
         return projectList
